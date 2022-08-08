a6888a164df830267dd80f217cdac1682cb8e925
Talison Antunes <talisonantunes.dev@gmail.com>
2022-08-08 17:47:38 -0300

    Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let tarefas = []
 
 
-
 function atualizar(element) {
   let tarefa = element.id;
   let tarefas_atualizadas = []
@@ -11,7 +10,6 @@ function atualizar(element) {
 
       tarefas[i].check = !tarefas[i].check
     }
-
     tarefas_atualizadas.push(tarefas[i])
   }
   localStorage.setItem("tarefas_salvas", JSON.stringify(tarefas_atualizadas))
@@ -20,7 +18,7 @@ function atualizar(element) {
 
 onload = function () {
   let div_tarefas = document.getElementById("tarefas");
-  let tarefas_localStorage = localStorage.getItem("tarefas_salvas")
+  let tarefas_localStorage = localStorage.getItem("tarefas_salvas");
 
   if (tarefas_localStorage != null) {
     tarefas = JSON.parse(tarefas_localStorage);
@@ -33,7 +31,7 @@ onload = function () {
         ${tarefas[i].conteudo} 
         </p>
         <button class="excluir" onclick="excluirTarefa('${tarefas[i].id}')"> 
-        <img class="lixeira" src="./assets/lixeira.png" alt="lixeira"></button> </div>`)
+        <img class="lixeira" src="./assets/lixeira.png" alt="lixeira"></button> </div>`);
     }
   }
 }
@@ -48,36 +46,39 @@ onload = function () {
 
 function criarTarefa() {
   let input = document.getElementById("novo");
-  let valor = input.value;
+  let valor = input.value.trim();
   let div_tarefas = document.getElementById("tarefas");
 
-  let tarefa = { id: Math.random(), conteudo: valor, check: false }
+  let tarefa = { id: Math.random(), conteudo: valor, check: false };
 
-  tarefas.push(tarefa)
+  input.value = ""
 
-  localStorage.setItem("tarefas_salvas", JSON.stringify(tarefas))
+  if (valor != "") {
 
-  return (div_tarefas.innerHTML += `<div class="tarefa"> 
+    (div_tarefas.innerHTML += `<div class="tarefa"> 
   <input type="checkbox" ${tarefa.check == true ? "checked" : ""
-    } onchange="atualizar(this)" id="${tarefa.id}">
+      } onchange="atualizar(this)" id="${tarefa.id}">
          <p> 
         ${tarefa.conteudo} 
         </p>
         <button class="excluir" onclick="excluirTarefa('${tarefa.id}')"> 
         <img class="lixeira" src="./assets/lixeira.png" alt="lixeira"></button> </div>`);
 
+    tarefas.push(tarefa)
+    localStorage.setItem("tarefas_salvas", JSON.stringify(tarefas));
 
+    input.className = "ok"
+  }
+  else {
+    input.className = "erro"
+  }
 
 }
-
-
-
 
 
 function excluirTarefa(element_id) {
   let tarefa_atual = document.getElementById(element_id);
   let div_pai = tarefa_atual.parentElement;
-
   div_pai.parentNode.removeChild(div_pai)
 
   let novas_tarefas = []
@@ -89,13 +90,9 @@ function excluirTarefa(element_id) {
 
     }
     else {
-    tarefas.splice(i, 1)
+      tarefas.splice(i, 1)
     }
-    
+
   }
   localStorage.setItem("tarefas_salvas", JSON.stringify(novas_tarefas));
-  
-
-
-
 }
